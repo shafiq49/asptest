60394f0c9ab6d02ad3f0d76dea72ffaa4086a419
shafiq49 <shafiqul.haque.milil@gmail.com>
2023-08-29 23:10:55 +0600

    Contact page created

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -25,26 +25,21 @@ namespace WebApplication2.Controllers
 
         public ActionResult Contact()
         {
-            string dt1, dt2;
-            dt1 = "01/05/2021";
-            dt2 = "01/05/2022";
-            var diffs = function.YearMonthDiff(Convert.ToDateTime(dt1), Convert.ToDateTime(dt2));
-            //Console.WriteLine("TotalMonths: " + diffs.TotalMonths);
-            //Console.WriteLine("Years: " + diffs.Years);
-            //Console.WriteLine("Months: " + diffs.Months);
-            DateTime xxs = Convert.ToDateTime("01/05/2022");
-            DateTime xxs1 = function.Make_DateSQL("01/05/2022");
-            ViewBag.Message = "Year is :" + diffs.Years + Environment.NewLine + "Month is : " + diffs.Months + Environment.NewLine + "Total Month is : " + diffs.TotalMonths + Environment.NewLine + "Date: " + xxs + Environment.NewLine + "Date: " + xxs1;
+            ViewBag.Message = "Hello Shafiq !!!";
+            var Contact = new Contact()
+            {
+                id = 1,
+                name = "Shafiq",
+                address= "Basabo, Dhaka",
+                mobile = 01313474849
 
-            return View();
+            };
+            
+
+            return View(Contact);
         }
 
-        public ActionResult User()
-        {
-            return View();
-
-        }
-       
+        
 
     }
 }
---
+++ WebApplication2/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Contact
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public int mobile { get; set; }
+          
+    }
+}
