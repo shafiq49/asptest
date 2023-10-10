dfb52238b2de2d5482273f38337ea1d65830458e
shafiq49 <shafiqul.haque.milil@gmail.com>
2023-10-10 07:35:10 +0600

    Buying House Application added

---
+++ BuyingHouseApplication/Migrations/202309241603275_addedCategoryTable1.cs
@@ -0,0 +1,16 @@
+namespace BuyingHouseApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedCategoryTable1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
