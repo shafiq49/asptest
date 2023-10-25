b0f9c7febec884077dfc0ece7351311ade5965b6
shafiq49 <shafiqul.haque.milil@gmail.com>
2023-10-25 23:11:48 +0600

    Tutorial 4 complited

--- WebApplication2/Models/IdentityModels.cs
+++ WebApplication2/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace WebApplication2.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Product>Products { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
--- WebApplication2/Models/Product.cs
+++ WebApplication2/Models/Product.cs
@@ -5,10 +5,9 @@ using System.Web;
 
 namespace WebApplication2.Models
 {
-    public class User
+    public class Product
     {
         public int Id { get; set; }
-        public string  Name { get; set; }
-        public int Age { get; set; }
+        public string Name { get; set; }
     }
 }
