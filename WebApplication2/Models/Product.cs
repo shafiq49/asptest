0553f9eade9e477eb1e243ac334c1bcdc46b1a90
Shafiqul Haque <shafiqul.haque.milil@gmail.com>
2023-10-26 08:35:51 +0600

    Categmory class created

--- WebApplication2/Models/Product.cs
+++ WebApplication2/Models/Product.cs
@@ -9,5 +9,13 @@ namespace WebApplication2.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Fabric { get; set; }
+        public string  Color { get; set; }
+        public int Quentity { get; set; }
+        public string Unitprice { get; set; }
+        public string Description { get; set; }
+        public Catagory Catagory { get; set }
+
+
     }
 }
